74c92de9168603f4cb6b3831e404df9e7a8c66e5
ixior462 <pawel.krupski@mail.com>
2023-06-30 00:56:16 +0200

    Add Fetching data from blockchain in cert-viewer

---
+++ cert-viewer-app/src/components/blockchain-fetcher.tsx
@@ -0,0 +1,77 @@
+
+import { AbiItem } from 'web3-utils'
+import Web3 from "web3";
+import Abi from './../certToken.json'
+import { keccak256, toUtf8Bytes } from "ethers/lib/utils";
+import { CertificateDTO } from "./cert-table";
+
+interface CertificateContract {
+    firstName: string;
+    hashedEmail: string;
+    lastName: string;
+    certType: string;
+    certLevel: string;
+    tokenId: string;
+}
+
+const mintedTokenURLPrefix = "https://cx04.westeurope.cloudapp.azure.com:4001/token/0x70c0b60e84bdeec72e855325521d7d51f105239f/instance/";
+const webServiceAdress = 'ws://cx04.westeurope.cloudapp.azure.com:8546';
+const smartContractAdress = "0x70C0B60E84BDeeC72E855325521d7D51F105239f";
+
+async function getResult(hashedEmail: string) {
+    var options = {
+        timeout: 30000,
+        // Useful if requests result are large
+        clientConfig: {
+            maxReceivedFrameSize: 100000000,   // bytes - default: 1MiB
+            maxReceivedMessageSize: 100000000, // bytes - default: 8MiB
+        },
+        // Enable auto reconnection
+        reconnect: {
+            auto: true,
+            delay: 5000, // ms
+            maxAttempts: 5,
+            onTimeout: false
+        }
+    };
+
+    var web3 = new Web3(new Web3.providers.WebsocketProvider(webServiceAdress, options));
+    const daiToken = new web3.eth.Contract(Abi as AbiItem[], smartContractAdress)
+    const certificantTokenIdsFunction = daiToken.methods.certificantTokenIds(hashedEmail)
+
+    let certsIds = await certificantTokenIdsFunction.call()
+
+    var certificates: CertificateContract[] = await Promise.all(certsIds.map(async (id: string): Promise<CertificateContract> => {
+        const tokenURIFuntion = daiToken.methods.tokenURI(id)
+        var certEncodedWithPrefix: string = await tokenURIFuntion.call()
+        var certEncoded: string = certEncodedWithPrefix.replace('data:application/json;base64,', '')
+        var cert: string = atob(certEncoded)
+        let certificateContract: CertificateContract = JSON.parse(cert);
+        certificateContract.tokenId = id
+        return certificateContract
+    }));
+
+    return certificates
+}
+
+
+export async function getCertificates(email: string) {
+    let hashedEmail = keccak256(toUtf8Bytes(email))
+
+    var certificateContracts: CertificateContract[] = await getResult(hashedEmail)
+
+    let certificates = certificateContracts.map(certificateContract => {
+        let certificate: CertificateDTO = {
+            name: certificateContract.firstName,
+            surname: certificateContract.lastName,
+            certType: certificateContract.certType,
+            certGrade: certificateContract.certLevel,
+            mintURL: mintedTokenURLPrefix + certificateContract.tokenId,
+            certLogoURL: "/" + certificateContract.certLevel + certificateContract.certType + ".png"
+        };
+
+        return certificate
+    })
+
+    return certificates
+}
--- cert-viewer-app/src/components/cert-table.tsx
+++ cert-viewer-app/src/components/cert-table.tsx
@@ -6,78 +6,89 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-  type CertificateDTO = {
-    name: string;
-    surname: string;
-    certType: string;
-    certGrade: string;
-    certLogoURL: string;
-  };
+import { useEffect, useState } from "react";
+import { getCertificates } from "./blockchain-fetcher";
 
-  
-  function getCertificates(email: string) {
-    const certificates: Array<CertificateDTO> = [];
+export type CertificateDTO = {
+  name: string;
+  surname: string;
+  certType: string;
+  certGrade: string;
+  mintURL: string;
+  certLogoURL: string;
+};
 
-    let certificate1: CertificateDTO = {
-        name: "name",
-        surname: "surname",
-        certType: "type1",
-        certGrade: "grade1",
-        certLogoURL: "https://cdn.iconscout.com/icon/premium/png-256-thumb/certificate-4449259-3687567.png?f=webp&w=128"
-      };
+export const CertTable = ({ email }: FormValues) => {
+  const certificateContracts: CertificateDTO[] = []
+  const [catUrl, certificates] = useState(certificateContracts);
+  const [error, setError] = useState(false);
+  const [state, setState] = useState('');
 
-      let certificate2: CertificateDTO = {
-        name: "name",
-        surname: "surname",
-        certType: "type2",
-        certGrade: "grade2",
-        certLogoURL: "https://cdn.iconscout.com/icon/premium/png-256-thumb/certificate-4449259-3687567.png?f=webp&w=128"
-      };
+  useEffect(() => {
+    setState('loading');
+    getCertificates(email)
+      .then((res) => {
+        setState('success');
+        certificates(res);
+      })
+      .catch((err) => {
+        console.error('Error:', err);
+        setState('error');
+        setError(err);
+      });
+  }, []);
 
-    certificates.push(certificate1)
-    certificates.push(certificate2)
-
-    return certificates
-  }
-  
-  export const CertTable = ({email}: FormValues) => {
-    let certificates = getCertificates(email)
-    return  <>
-              <Box sx={{
-                marginTop: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center'}}>
-              <Typography component="h1" variant="h5" paddingBottom={3}>
-                  {email}
-              </Typography>
-              <TableContainer component={Paper} variant="outlined">
-                <Table sx={{ minWidth: 650, borderRadius: '0', elevation: 0, }} aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell align="left">Name</TableCell>
-                      <TableCell align="left">Surname</TableCell>
-                      <TableCell align="left">Certificate Type</TableCell>
-                      <TableCell align="left">Certificate Grade</TableCell>
-                      <TableCell align="left">Logo</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {certificates.map((row) => (
-                      <TableRow
-                        key={row.certType + row.certGrade}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                      >
-                        <TableCell align="left">{row.name}</TableCell>
-                        <TableCell align="left">{row.surname}</TableCell>
-                        <TableCell align="left">{row.certType}</TableCell>
-                        <TableCell align="left">{row.certGrade}</TableCell>
-                        <TableCell align="left"><img src={row.certLogoURL}></img></TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-              </Box>
-            </>;
-  };
+  if (state === 'error')
+    return (
+      <h1>
+        {error.toString()}
+      </h1>
+    );
+  return (
+    <>
+      {state === 'loading' ? (
+        <div>Loading...</div>
+      ) : (
+        <Box sx={{
+          marginTop: 2,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}>
+          <Typography component="h1" variant="h5" paddingBottom={3}>
+            {email}
+          </Typography>
+          <TableContainer component={Paper} variant="outlined">
+            <Table sx={{ minWidth: 650, borderRadius: '0', elevation: 0, }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="left">Name</TableCell>
+                  <TableCell align="left">Surname</TableCell>
+                  <TableCell align="left">Certificate Type</TableCell>
+                  <TableCell align="left">Certificate Grade</TableCell>
+                  <TableCell align="left">Minted NFT</TableCell>
+                  <TableCell align="left">Logo</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {catUrl.map((row) => (
+                  <TableRow
+                    key={row.certType + row.certGrade}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell align="left">{row.name}</TableCell>
+                    <TableCell align="left">{row.surname}</TableCell>
+                    <TableCell align="left">{row.certType}</TableCell>
+                    <TableCell align="left">{row.certGrade}</TableCell>
+                    <TableCell align="left"><a href={row.mintURL}>Link</a></TableCell>
+                    <TableCell align="left"><img src={row.certLogoURL} width={300} height={'auto'}></img></TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      )}
+    </>
+  );
+};
