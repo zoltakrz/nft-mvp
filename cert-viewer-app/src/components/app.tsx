a0ec543c8cc4c913119b9a716ee5cda7557a0877
AZOLTANS <anna.zoltanska@capgemini.com>
2023-08-14 23:02:59 +0200

    MetaMask login

--- cert-viewer-app/src/components/app.tsx
+++ cert-viewer-app/src/components/app.tsx
@@ -1,9 +1,18 @@
 import { Container, CssBaseline } from '@mui/material';
 import './../styles.css';
 import { EmailForm } from "./email-form";
+import ReactGA from 'react-ga';
+import React, {useEffect } from 'react';
 
+const TRACKING_ID = "G-RT6QSME5CP"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
+
   return (
     <Container component="main">
       <CssBaseline />
