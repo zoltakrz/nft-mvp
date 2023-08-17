5a702a364be46922092b8bcf400ca87610d48e64
AZOLTANS <anna.zoltanska@capgemini.com>
2023-08-17 12:40:38 +0200

    Google Analytics and Tags config

--- cert-viewer-app/src/components/app.tsx
+++ cert-viewer-app/src/components/app.tsx
@@ -3,9 +3,15 @@ import './../styles.css';
 import { EmailForm } from "./email-form";
 import ReactGA from 'react-ga';
 import React, {useEffect } from 'react';
+import TagManager from 'react-gtm-module'
 
 const TRACKING_ID = "G-RT6QSME5CP"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
+
+const tagManagerArgs = {
+    gtmId: '<G-RT6QSME5CP>'
+}
+TagManager.initialize(tagManagerArgs)
 
 function App() {
 
